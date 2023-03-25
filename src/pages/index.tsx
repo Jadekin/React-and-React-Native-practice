0369de68fe1baac83f3eb32bc3f63d88f7763195
Karen Pinzás Morrongiello <karen.pinzasmorrongiello@freshbooks.com>
2023-03-25 19:17:29 -0400

    Using fragments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,32 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-const enabled = false;
-const text = "Here is the button";
-const placeholder = "This is the placeholder";
-
-const array = ["First", "Second", "Third"];
-
-const object = {
-  first: 1,
-  second: 2,
-  third: 3,
-};
 
 export default function Home() {
   return (
     <section>
-      <h1>Test of mapping collection to elements</h1>
-      {array.map((i) => (
-        <>
-          <button key={i} disabled={enabled}>
-            {i}
-          </button>
-        </>
-      ))}
-
-      {Object.keys(object).map((i) => (
-        <input key={`${i}3`} placeholder={object[i]}></input>
-      ))}
+      <WithoutFragments />
+      <WithFragments />
     </section>
+  );
+}
+
+export function WithoutFragments() {
+  return (
+    <div>
+      <h1>Without Fragments</h1>
+      <p>
+        Here hereh with <code>div</code>
+      </p>
+    </div>
+  );
+}
+
+export function WithFragments() {
+  return (
+    <>
+      <h1>Without Fragments</h1>
+      <p>
+        Here hereh with <code>div</code>
+      </p>
+    </>
   );
 }

3547c5eb901f7676e7688e50b35b764f9718e29d
Karen Pinzás Morrongiello <karen.pinzasmorrongiello@freshbooks.com>
2023-03-25 19:10:26 -0400

    Test of mapping collection to elements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,29 @@ const enabled = false;
 const text = "Here is the button";
 const placeholder = "This is the placeholder";
 
+const array = ["First", "Second", "Third"];
+
+const object = {
+  first: 1,
+  second: 2,
+  third: 3,
+};
+
 export default function Home() {
   return (
     <section>
-      <button disabled={enabled}>{text}</button>
-      <input placeholder={placeholder}></input>
+      <h1>Test of mapping collection to elements</h1>
+      {array.map((i) => (
+        <>
+          <button key={i} disabled={enabled}>
+            {i}
+          </button>
+        </>
+      ))}
+
+      {Object.keys(object).map((i) => (
+        <input key={`${i}3`} placeholder={object[i]}></input>
+      ))}
     </section>
   );
 }

7058da4ab14bb4daacfd6484c20e6a1af1773288
Karen Pinzás Morrongiello <karen.pinzasmorrongiello@freshbooks.com>
2023-03-25 20:20:04 -0400

    Button as functional component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,58 +1,9 @@
 import * as React from "react";
 
-class MyComponent extends React.Component {
-  state = {
-    first: "loading...",
-    second: "loading...",
-    third: "loading...",
-    fourth: "loading...",
-    doneMessage: "Doneeee!!",
-  };
-
-  constructor() {
-    super();
-
-    setTimeout(() => {
-      this.setState({
-        first: "Done! :)",
-      });
-    }, 1000);
-
-    setTimeout(() => {
-      this.setState({
-        second: "Done! :)",
-      });
-    }, 2000);
-
-    setTimeout(() => {
-      this.setState({
-        ...this.state,
-        third: "Done! :)",
-      });
-    }, 3000);
-
-    setTimeout(() => {
-      this.setState({
-        fourth: this.state.doneMessage,
-      });
-    }, 4000);
-  }
-
-  render() {
-    return (
-      <main>
-        <ul>
-          {Object.keys(this.state)
-            .filter((key) => key !== "doneMessage")
-            .map((key) => (
-              <li key={key}>
-                <strong>{key}</strong>: {this.state[key]}
-              </li>
-            ))}
-        </ul>
-      </main>
-    );
-  }
+export default function ShowButton() {
+  return <Button disabled={false} text="This is the button" />;
 }
 
-export default MyComponent;
+export function Button({ disabled, text }) {
+  return <button disabled={disabled}> {text}</button>;
+}

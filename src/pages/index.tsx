8fe93d7a43e7eb15331037da830b7b9385ee1674
Karen Pinzás Morrongiello <karen.pinzasmorrongiello@freshbooks.com>
2023-03-28 12:27:14 -0400

    useState hook + interface props that are optional

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,40 @@
 import * as React from "react";
 
 export default function Home() {
+  const [name, setName] = React.useState("Karen");
+  const [age, setAge] = React.useState(35);
+
   return (
     <>
-      <Button disabled={true} text="This is the button" />
+      <div>
+        <input value={name} onChange={(e) => setName(e.target.value)}></input>
+      </div>
+      <div>
+        <input
+          type="number"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}></input>
+      </div>
+      <div>
+        <Button disabled={false} text="Change name" />
+      </div>
+
+      <p>
+        This is {name} and I am {age}{" "}
+      </p>
     </>
   );
 }
 export interface Props {
   disabled: boolean;
   text: string;
+  onClick?: React.FunctionComponent<React.MouseEvent<HTMLButtonElement>>;
 }
 
-export function Button({ disabled, text }: Props) {
-  return <button disabled={disabled}> {text}</button>;
+export function Button({ disabled, text, onClick }: Props) {
+  return (
+    <button disabled={disabled} onClick={onClick}>
+      {text}
+    </button>
+  );
 } 
